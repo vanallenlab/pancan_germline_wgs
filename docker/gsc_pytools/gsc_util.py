b4c95b4288e89e04e21749ef5c80c9e56bf1d0ed
noahfields1 <noah.fields253@gmail.com>
2024-09-25 15:48:31 -0400

    new docker container: gsc_pytools

--- docker/gsc_pytools/gsc_util.py
+++ docker/gsc_pytools/gsc_util.py
@@ -100,7 +100,9 @@ def get_unique_combinations_nc_c(tsv_file,germline_context,somatic_context):
     return unique_combinations
 
 
-def fishers_exact(df, germline_gene,somatic_gene):
+def fishers_exact(df, cancer_type, germline_gene,somatic_gene):
+    if cancer_type != "pancancer":
+        df = df[df['cancer_type'] == cancer_type]
     germline_gene_full_name = germline_gene + '-g'
     somatic_gene_full_name = somatic_gene + '-s'
 
