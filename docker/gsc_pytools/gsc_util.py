fce8c69c0aa6132f2b1e334e68634e2a162a76b0
noahfields1 <noah.fields253@gmail.com>
2024-09-26 14:44:37 -0400

    fixing gsc_pytools docker

--- docker/gsc_pytools/gsc_util.py
+++ docker/gsc_pytools/gsc_util.py
@@ -5,6 +5,7 @@ import numpy as np
 from firthlogist import FirthLogisticRegression
 
 def firth_logistic_regression(df, cancer_type, germline_event, somatic_gene):
+    print(f"Cancer Type: {cancer_type}\n Germline Event: {germline_event} \n Somatic Gene: {somatic_gene}")
     if cancer_type != "Pancancer":
         df = df[df['cancer_type'] == cancer_type]
 
@@ -23,13 +24,6 @@ def firth_logistic_regression(df, cancer_type, germline_event, somatic_gene):
     if somatic_gene not in df.columns:
         print(f"somatic_gene {somatic_gene} not found in DataFrame columns")
         return
-
-    # Filter out rows with NA values in germline or somatic gene columns
-    df_filtered = df.dropna(subset=[germline_event, somatic_gene])
-    
-    if df_filtered[germline_event].sum() == 0 or df_filtered[somatic_gene].sum() == 0:
-        #print(f"No germline_gene: {germline_gene}")
-        return
     
     try:
         # Prepare the predictor (X) and response (y) variables
@@ -47,12 +41,12 @@ def firth_logistic_regression(df, cancer_type, germline_event, somatic_gene):
         model.fit(X,y)
         
         # Extract p-value and odds ratio for the germline_gene predictor
-        p_value = result.pvalues[germline_event]
-        odds_ratio = np.exp(result.params[germline_event])
+        p_value = model.pvalues[germline_event]
+        odds_ratio = np.exp(model.params[germline_event])
         
         # Calculate the 95% confidence interval for the odds ratio
-        coef = result.params[germline_event]
-        std_err = result.bse[germline_event]
+        coef = model.params[germline_event]
+        std_err = model.bse[germline_event]
 
         # The confidence interval for the coefficient
         conf_int_coef = [coef - 1.96 * std_err, coef + 1.96 * std_err]
@@ -66,73 +60,6 @@ def firth_logistic_regression(df, cancer_type, germline_event, somatic_gene):
     except Exception as e:
         print(f"Error processing combination {germline_event} - {somatic_gene}: {e}")
 
-
-def fishers_exact(df, cancer_type, germline_gene,somatic_gene):
-    if cancer_type != "Pancancer":
-        df = df[df['cancer_type'] == cancer_type]
-    germline_gene_full_name = germline_gene + '-g'
-    somatic_gene_full_name = somatic_gene + '-s'
-
-    if germline_gene_full_name not in df.columns or somatic_gene_full_name not in df.columns:       
-        print(f"Combination {germline_gene} - {somatic_gene} not found in DataFrame")
-        return
-    if df[germline_gene_full_name].sum() == 0 or df[somatic_gene_full_name].sum() == 0:
-        return
-
-    try:
-        # Create a 2x2 contingency table manually
-        table = [
-            [
-                df[(df[germline_gene_full_name] == 0) & (df[somatic_gene_full_name] == 0)].shape[0],
-                df[(df[germline_gene_full_name] == 0) & (df[somatic_gene_full_name] == 1)].shape[0]
-            ],
-            [
-                df[(df[germline_gene_full_name] == 1) & (df[somatic_gene_full_name] == 0)].shape[0],
-                df[(df[germline_gene_full_name] == 1) & (df[somatic_gene_full_name] == 1)].shape[0]
-            ]
-        ]
-
-        
-        # Perform Fisher's exact test
-        odds_ratio, p_value = fisher_exact(table)
-
-        # Check if odds_ratio is zero
-        if odds_ratio == 0:
-            log_or = -np.inf  # Use negative infinity for log odds ratio
-        else:
-            log_or = np.log(odds_ratio)
-
-        # Convert the table to a NumPy array
-        table_array = np.array(table)
-
-        # Standard error approximation for the log odds ratio
-        a, b, c, d = table_array.ravel()
-        if a == 0 or b == 0 or c == 0 or d == 0:
-            epsilon = 0.01
-            a += epsilon
-            b += epsilon
-            c += epsilon
-            d += epsilon
-
-        se = np.sqrt(1/a + 1/b + 1/c + 1/d)
-
-        # Compute 95% confidence interval for log(odds ratio)
-        log_ci_low = log_or - 1.96 * se
-        log_ci_high = log_or + 1.96 * se
-
-        # If log_or is -inf, ci_low will be very negative and should be handled
-        if log_or == -np.inf:
-            ci_low = 0  # You might set the lower bound of the CI to 0
-            ci_high = 0  # Or set it to something reasonable, e.g., 0
-        else:
-            ci_low = np.exp(log_ci_low)
-            ci_high = np.exp(log_ci_high)
-
-        return odds_ratio, p_value, ci_low, ci_high
-        
-    except Exception as e:
-        print(f"Error occurred for combination {germline_gene} & {somatic_gene}: {str(e)}")
-        return
 
 def find_allele_frequency(df, column_name):
     """
