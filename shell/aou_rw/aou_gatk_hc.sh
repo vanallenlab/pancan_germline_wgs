3b7dcb1d6bb34b4370ce9f24c1e177e49e62ca3d
Ryan Collins <ryan.l.collins.13@gmail.com>
2025-02-14 09:39:37 -0500

    Initial setup for GATK-HC joint genotyping

---
+++ shell/aou_rw/aou_gatk_hc.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# The Germline Genomics of Cancer (G2C)
+# Copyright (c) 2024-Present, Ryan L. Collins and the Dana-Farber Cancer Institute
+# Contact: Ryan Collins <Ryan_Collins@dfci.harvard.edu>
+# Distributed under the terms of the GNU GPL v2.0
+
+# Shell code to run GATK-HC joint genotyping on G2C phase 1
+
+# Note that this code is designed to be run inside the AoU Researcher Workbench
+
+
+#########
+# SETUP #
+#########
+
+# Set up local environment
+export GPROJECT="vanallen-pancan-germline-wgs"
+export MAIN_WORKSPACE_BUCKET=gs://fc-secure-d21aa6b0-1d19-42dc-93e3-42de3578da45
+
+# Prep working directory structure
+for dir in cromshell cromshell/inputs cromshell/job_ids cromshell/progress; do
+  if ! [ -e $dir ]; then
+    mkdir $dir
+  fi
+done
+
+# Copy necessary code to local disk
+gsutil -m cp -r $MAIN_WORKSPACE_BUCKET/code ./
+find code/ -name "*.py" | xargs -I {} chmod a+x {}
+find code/ -name "*.R" | xargs -I {} chmod a+x {}
+
