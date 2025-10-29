d45634e36a3ef683b6ffeaf3d037bb1b28dd4d88
Ryan Collins <ryan.l.collins.13@gmail.com>
2025-10-29 10:29:01 -0400

    Initialize new branch

--- shell/aou_rw/aou_prep_wdls.sh
+++ shell/aou_rw/aou_prep_wdls.sh
@@ -19,7 +19,7 @@ WRKDIR=`mktemp -d`
 cd $WRKDIR
 
 # Clone G2C repo & checkout branch of interest
-export g2c_branch=main
+export g2c_branch=posthoc_filtering
 git clone git@github.com:vanallenlab/pancan_germline_wgs.git --branch=$g2c_branch
 
 # Organize G2C WDLs
